0ee732cddc7c883b81d8190109dfa4657160a23d
Stephen Castaneda <stephen.c976@gmail.com>
2017-01-27 11:12:36 -0700

    Solved 'match_ends' exercise

--- basic/list1.py
+++ basic/list1.py
@@ -22,7 +22,11 @@
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
   # +++your code here+++
-  return
+  return_count = 0
+  for word in words:
+    if len(word) >= 2 and word[0] == word[-1] :
+      return_count += 1
+  return (return_count)
 
 
 # B. front_x
