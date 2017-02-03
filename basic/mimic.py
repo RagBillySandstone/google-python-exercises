eab7037e7ce74a0c456a38a74b584108a738c026
Stephen Castaneda <stephen.c976@gmail.com>
2017-02-03 13:47:07 -0700

    Completed mimic.py exercise

--- basic/mimic.py
+++ basic/mimic.py
@@ -48,12 +48,30 @@ import sys
 def mimic_dict(filename):
   """Returns mimic dict mapping each word to list of words which follow it."""
   # +++your code here+++
-  return
+  mimic_dict = {}
+  with open(filename, 'r') as f:
+    text = f.read()
+    words = text.split()
+    prev = ''
+    for word in words:
+      if not prev in mimic_dict:
+        mimic_dict[prev] = [word]
+      else:
+        mimic_dict[prev].append(word)
+      prev = word
+    return mimic_dict
 
 
 def print_mimic(mimic_dict, word):
   """Given mimic dict and start word, prints 200 random words."""
   # +++your code here+++
+  for i in range(200):
+    print word,
+    nexts = mimic_dict.get(word)
+    if not nexts:
+      nexts = mimic.dict['']
+    word = random.choice(nexts)
+
   return
 
 
